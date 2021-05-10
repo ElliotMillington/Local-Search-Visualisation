fddc056902d7d64e1dbeb4063a20051cfd0624ac
Frantisek Bartos <f.bartos96@gmail.com>
2021-05-11 00:58:49 +0200

    cleaning the code + fixing bugs

--- intro_tab.R
+++
@@ -1,6 +0,0 @@
-### intro_tab ----
-intro_tab <- tabItem(
-  tabName = "intro_tab",
-  h3("Introduction"),
-  p("This app is designed to help you understand the algorithms used in statistical estimation.")
-)
